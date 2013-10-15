091ed95e3832956fed2c3a54422764bdc22f16f5
Brian Loomis <brian@customated.com>
2013-10-14 20:04:24 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Timeslips::Application.config.secret_key_base = '2be7027af28205370ab68393f75717855ccf17440d741fefd46038eb33a81122c6292a2a276c80a2241baea7b18786e9a0a7b64d7c13bb69b0e2218f5583b1e9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Timeslips::Application.config.session_store :cookie_store, key: '_timeslips_session'
